9a400f71eb1e5e2482cbdb4b7782c9f9244d72d9
Pavel Smagin <paulsmagin@gmail.com>
2019-04-22 19:43:56 +0300

    remove readfile

--- get_book.py
+++ get_book.py
@@ -1,14 +1,17 @@
 from PyPDF2 import PdfFileReader, PdfFileWriter
+
 output = PdfFileWriter()
 
 
 def get(path, pages: list, output_path):
-    with open(path, 'r') as readfile:
-        file = PdfFileReader(path)
-        for list in pages:
-            page = file.getPage(list)
-            with open(output_path, "wb") as writefile:
-                insert = output.insertPage(page=page, index=pages.index(list))
-                output.write(writefile)
+    file = PdfFileReader(path)
+
+    for list in pages:
+        page = file.getPage(list)
+
+        with open(output_path, "wb") as writefile:
+            insert = output.insertPage(page=page, index=pages.index(list))
+            output.write(writefile)
+
     print("Sorting successfully done!")
     return None
